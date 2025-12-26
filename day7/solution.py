d9c959bb5e116200db8f1a8182809f674582ceb3
Caleb Fringer <cfringer98@gmail.com>
2025-12-26 15:14:04 -0800

    Solve day 7 part 2.

--- day7/solution.py
+++ day7/solution.py
@@ -30,3 +30,35 @@ def partOne(filepath):
 
 partOne("test.txt")  # 21
 partOne("input.txt")  # 1490
+
+
+def preprocessTwo(filepath):
+    with open(filepath) as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def partTwo(filepath):
+    rows = preprocessTwo(filepath)
+
+    cache = {}
+
+    def dfs(pos):
+        nonlocal rows
+        i, j = pos
+        if i >= len(rows):
+            return 1
+        if i < 0 or j < 0 or j >= len(rows[0]):
+            return 0
+        if rows[i][j] == "^":
+            return dfs((i, j-1)) + dfs((i, j+1))
+
+        if (i+1, j) not in cache:
+            cache[(i+1, j)] = dfs((i+1, j))
+        return cache[(i+1, j)]
+
+    start = rows[0].find("S")
+    return dfs((0, start))
+
+
+partTwo("test.txt")  # 40
+partTwo("input.txt")  # 3806264447357
